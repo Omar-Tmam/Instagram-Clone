50eec86574c7994ae7e218193b764f989caf28d0
omartmam <omartmam9@gmail.com>
2025-09-01 16:20:57 +0300

    add responsive font size

---
+++ lib/Core/utils/responsive_font_size.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/Core/utils/size_config.dart';
+
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  // var dispatcher = PlatformDispatcher.instance;
+  // var physicalWidth = dispatcher.views.first.physicalSize.width;
+  // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  // var width = physicalWidth / devicePixelRatio;
+  double width = MediaQuery.sizeOf(context).width;
+
+  if (width < SizeConfig.tablet) {
+    return width / 400;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1200;
+  } else {
+    return width / 1900;
+  }
+}
