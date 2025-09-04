fc955e8c13409d401f3fd2587f53567fe30606ea
omartmam <omartmam9@gmail.com>
2025-09-04 16:58:21 +0300

    edit android mainfest and add video player widget

--- lib/Features/video_view/presentation/views/widgets/user_video_view_body.dart
+++ lib/Features/video_view/presentation/views/widgets/user_video_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/Core/utils/functions/user_data_args.dart';
 import 'package:instagram_clone/Features/user_view/data/models/reels_model/item.dart';
+import 'package:instagram_clone/Features/video_view/presentation/views/widgets/video_player.dart';
+import 'package:video_player/video_player.dart';
 
 class UserVideoViewBody extends StatelessWidget {
   const UserVideoViewBody({super.key, required this.item});
@@ -8,6 +10,8 @@ class UserVideoViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: MyVideoPlayer(),
+    );
   }
 }
---
+++ lib/Features/video_view/presentation/views/widgets/video_player.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyVideoPlayer extends StatefulWidget {
+  const MyVideoPlayer({super.key});
+
+  @override
+  State<MyVideoPlayer> createState() => _MyVideoPlayerState();
+}
+
+class _MyVideoPlayerState extends State<MyVideoPlayer> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
